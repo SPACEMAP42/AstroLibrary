9bac061b1255d7001f8fe7dad1f085c4f8e16226
김종원 <labora7@naver.com>
2023-04-07 18:12:40 +0900

    Feat: create session cookie

---
+++ astrolibrary/functions/ppdb/api.py
@@ -0,0 +1,6 @@
+class ppdb:
+    def __init__(self):
+        pass
+
+    def get_ppdb_conjunctions():
+        pass
--- astrolibrary/main.py
+++ astrolibrary/main.py
@@ -1,6 +1,18 @@
-import functions.tle as TLE
-
+import requests
+import configparser
+from functions.ppdb.api import ppdb
+from functions.token_auth.api import token_auth
 
 class AstroLib:
     def __init__(self, token):
-        self.tle = TLE
+        config = configparser.ConfigParser()
+        config.read('./config.ini')
+        self.base_url = config.get('SPACEMAP', 'BASE_URL')
+        self.token = token
+        self.session = requests.Session()
+        self.token_auth = token_auth(self.base_url,  self.session, self.token)
+
+
+if __name__ == "__main__":
+    astroLib = AstroLib("Y8HSpeoKt+10sYVL7pRJum2lBg8XFfWOu+LVyN0Y26+5l7EO3WXTbGipnlkgkmPi")
+    astroLib.token_auth.create_session_cookie()
