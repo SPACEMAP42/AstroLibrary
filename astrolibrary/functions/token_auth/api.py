fd7a83bb62d7d90221613ef2254b85da080ed2da
김종원 <labora7@naver.com>
2023-04-11 13:11:48 +0900

    Feat: poetry build & set python black

--- astrolibrary/functions/token_auth/api.py
+++ astrolibrary/functions/token_auth/api.py
@@ -1,17 +1,12 @@
 class token_auth:
     def __init__(self, base_url, session, token):
-        self.base_url = base_url
-        self.session = session
-        self.token = token
+        self.__base_url = base_url
+        self.__session = session
+        self.__token = token
 
-    def create_session_cookie(self):
-        endpoint = '/token-auth/token'
-        url = self.base_url + endpoint
-        headers = {
-            'Content-Type': 'application/x-www-form-urlencoded',
-        }
-        payload = {
-            'authToken': self.token,
-        }
-        self.session.post(url, headers=headers,data=payload)
-        # print(self.session.cookies.get_dict())
+    def create_session(self):
+        endpoint = "/token-auth/token"
+        url = self.__base_url + endpoint
+        headers = {"Content-Type": "application/x-www-form-urlencoded"}
+        payload = {"authToken": self.__token}
+        self.__session.post(url, headers=headers, data=payload)
--- astrolibrary/main.py
+++ astrolibrary/main.py
@@ -1,17 +1,24 @@
 import requests
 import configparser
-from functions.ppdb.api import ppdb
-from functions.token_auth.api import token_auth
+from astrolibrary.functions.conjunction.api import conjunction
+from astrolibrary.functions.token_auth.api import token_auth
 
-class AstroLib:
+
+class client:
     def __init__(self, token):
         __config = configparser.ConfigParser()
-        __config.read('./config.ini')
-        self.base_url = __config.get('SPACEMAP', 'BASE_URL')
+        __config.read("./astrolibrary/config.ini")
+        self.__base_url = __config.get("SPACEMAP", "BASE_URL")
         self.__token = token
         self.__session = requests.Session()
-        self.token_auth = token_auth(self.base_url, self.__session, self.__token)
+        self.token_auth = token_auth(self.__base_url, self.__session, self.__token)
+        self.conjunction = conjunction(self.__base_url, self.__session)
 
-if __name__ == "__main__":
-    astroLib = AstroLib("Y8HSpeoKt+10sYVL7pRJum2lBg8XFfWOu+LVyN0Y26+5l7EO3WXTbGipnlkgkmPi")
-    astroLib.token_auth.create_session_cookie()
+
+# for testing astrolib using local environment !!
+# if __name__ == "__main__":
+#     spacemap = client(
+#         "Y8HSpeoKt+10sYVL7pRJum2lBg8XFfWOu+LVyN0Y26+5l7EO3WXTbGipnlkgkmPi"
+#     )
+#     spacemap.token_auth.create_session()
+#     print(spacemap.conjunction.get_conjunctions(5,12323232))
