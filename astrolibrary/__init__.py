008b8e5d092dec03a2622b34762875ab2d2c1a20
shawn17 <shchoi.vdrc@gmail.com>
2023-04-12 11:29:04 +0900

    Feat: concept of visualization tool

--- astrolibrary/__init__.py
+++ astrolibrary/__init__.py
@@ -3,8 +3,8 @@ __version__ = "0.1.0"
 from .main import Client
 from .functions.conjunction.api import conjunction
 from .functions.token_auth.api import token_auth
-from .functions.tle.api import tle
-from .graphic.visualization import
+from .functions.tle.api import Tle
+from .graphic.visualizationtool import VisualizationTool
 
 
-__all__ = ["client", "conjunction", "token_auth", "tle"]
+__all__ = ["Client", "conjunction", "token_auth", "Tle"]
---
+++ astrolibrary/graphic/visualizationtool.py
@@ -0,0 +1,3 @@
+class VisualizationTool:
+    def __init__(self):
+        pass
