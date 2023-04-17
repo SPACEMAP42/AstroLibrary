34498a560e1e2d0b018143e855395e1edf78d9d5
김종원 <labora7@naver.com>
2023-04-17 16:53:17 +0900

    Refactor: library structure & conjunction example code

--- astrolibrary/data/conjunction.py
+++ astrolibrary/data/conjunction.py
@@ -1,3 +1,6 @@
+import json
+
+
 class Conjunction:
     def __init__(self, conjunction):
         self.__created_at = conjunction["createdAt"]
@@ -16,17 +19,32 @@ class Conjunction:
     def p_id(self):
         return self.__p_id
 
+    # def __repr__(self):
+    #     return f"\n\
+    #         created_at: {self.__created_at}\n\
+    #         p_id: {self.__p_id}\n\
+    #         p_name: {self.__p_name}\n\
+    #         s_id: {self.__s_id}\n\
+    #         s_name: {self.__s_name}\n\
+    #         dca: {self.__dca}\n\
+    #         tca_time: {self.__tca_time}\n\
+    #         tca_start_time: {self.__tca_start_time}\n\
+    #         tca_end_time: {self.__tca_end_time}\n\
+    #         standard_time: {self.__standard_time}\n\
+    #         probability: {self.__probability}"
+
     def __repr__(self):
-        return f"\
-            created_at: {self.__created_at}\n\
-            p_id: {self.__p_id}\n\
-            p_name: {self.__p_name}\n\
-            s_id: {self.__s_id}\n\
-            s_name: {self.__s_name}\n\
-            dca: {self.__dca}\n\
-            tca_time: {self.__tca_time}\n\
-            tca_start_time: {self.__tca_start_time}\n\
-            tca_end_time: {self.__tca_end_time}\n\
-            standard_time: {self.__standard_time}\n\
-            probability: {self.__probability}\n\
-        "
+        data = {
+            "created_at": self.__created_at,
+            "p_id": self.__p_id,
+            "p_name": self.__p_name,
+            "s_id": self.__s_id,
+            "s_name": self.__s_name,
+            "dca": self.__dca,
+            "tca_time": self.__tca_time,
+            "tca_start_time": self.__tca_start_time,
+            "tca_end_time": self.__tca_end_time,
+            "standard_time": self.__standard_time,
+            "probability": self.__probability,
+        }
+        return json.dumps(data, indent=4)
--- astrolibrary/main.py
+++ astrolibrary/main.py
@@ -1,25 +1,31 @@
 import os
 import requests
 import configparser
-from .functions.conjunction.api import ConjunctionAPI
-from .functions.token_auth.api import TokenAuthAPI
-from .functions.tle.api import TLEAPI
-from .functions.watcher_catcher.api import WatcherCatcherAPI
+from .apis.conjunction.api import ConjunctionAPI
+from .apis.token_auth.api import TokenAuthAPI
+from .apis.tle.api import TLEAPI
+from .apis.watcher_catcher.api import WatcherCatcherAPI
 
 
 class Client:
     def __init__(self, token):
+        # save base url
         __config = configparser.ConfigParser()
         config_path = os.path.join(
             os.path.dirname(os.path.abspath(__file__)), "config.ini"
         )
         __config.read(config_path)
         self.__base_url = __config.get("SPACEMAP", "BASE_URL")
+
+        # create http session
         self.__token = token
         self.__session = requests.Session()
         self.token_auth_API = TokenAuthAPI(
             self.__base_url, self.__session, self.__token
         )
+        self.token_auth_API.create_session()
+
+        # create api member variables
         self.conjunction_API = ConjunctionAPI(self.__base_url, self.__session)
         self.tle_API = TLEAPI(self.__base_url, self.__session)
         self.watcher_catcher_API = WatcherCatcherAPI(self.__base_url, self.__session)
