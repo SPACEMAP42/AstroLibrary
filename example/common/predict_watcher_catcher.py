b96f8a795220daabebab8f4f6c8c56aaed0a5668
Seunghwan17 <shchoi.vdrc@gmail.com>
2023-05-03 20:52:10 +0900

    Fix: examples

--- example/common/predict_watcher_catcher.py
+++ example/common/predict_watcher_catcher.py
@@ -8,13 +8,11 @@
 
 import astrolibrary
 
-example_access_token = (
-    "Y8HSpeoKt+10sYVL7pRJum2lBg8XFfWOu+LVyN0Y26+5l7EO3WXTbGipnlkgkmPi"
-)
+example_access_token = ""
 
 if __name__ == "__main__":
-    # create an astrolibrary client named ROK_airforce
-    ROK_airforce = astrolibrary.Client(example_access_token)
+    # create an astrolibrary client named SPACEMAP
+    SPACEMAP = astrolibrary.Client(example_access_token)
 
     """
     api to predict space objects observable from any location on the ground
@@ -75,20 +73,20 @@ if __name__ == "__main__":
 
     # 1. call api with default parameters
     # send request to watcher catcher server
-    response = ROK_airforce.watcher_catcher_API.predict_watcher_catcher()
+    response = SPACEMAP.watcher_catcher_API.predict_watcher_catcher()
 
     # # 2. list of statuses of requests sent to the watcher catcher server
-    request_list = ROK_airforce.watcher_catcher_API.get_requests_status_list()["data"]
+    request_list = SPACEMAP.watcher_catcher_API.get_requests_status_list()["data"]
     print(request_list)
 
     # 3. Select a very last request from the list and read data
     # Save the database id, then read the value from that database
     # If you want to get the previous result rather than the last one, you can query the database.
-    id = ROK_airforce.watcher_catcher_API.get_requests_status_list()["data"][-1]["_id"]
-    print(ROK_airforce.watcher_catcher_API.get_predicted_result(id))
+    id = SPACEMAP.watcher_catcher_API.get_requests_status_list()["data"][-1]["_id"]
+    print(SPACEMAP.watcher_catcher_API.get_predicted_result(id))
 
     # API to clear the id of a specific database from the list
-    ROK_airforce.watcher_catcher_API.delete_predicted_result(id)
+    SPACEMAP.watcher_catcher_API.delete_predicted_result(id)
 
     # # A function that performs steps 1-3 above at once
-    print(ROK_airforce.watcher_catcher_API.predict_watcher_catcher_and_get_result())
+    print(SPACEMAP.watcher_catcher_API.predict_watcher_catcher_and_get_result())
