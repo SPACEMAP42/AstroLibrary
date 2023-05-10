e9482b4e5bcd04537427ecc344d25798268d7de2
Seunghwan17 <shchoi.vdrc@gmail.com>
2023-05-10 17:27:29 +0900

    Feat: watchercatcher table

--- example/pyside/opengl_ui.py
+++ example/pyside/opengl_ui.py
@@ -15,10 +15,10 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QDateTimeEdit, QGridLayout, QHBoxLayout,
+from PySide6.QtWidgets import (QApplication, QDateTimeEdit, QHBoxLayout, QHeaderView,
     QLabel, QMainWindow, QMenuBar, QPushButton,
-    QSizePolicy, QSpacerItem, QStatusBar, QVBoxLayout,
-    QWidget)
+    QSizePolicy, QSpacerItem, QStatusBar, QTabWidget,
+    QTableView, QVBoxLayout, QWidget)
 
 from astrodisplay import AstroDisplay
 
@@ -26,11 +26,13 @@ class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         if not MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
-        MainWindow.resize(1240, 976)
+        MainWindow.resize(1348, 1044)
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
-        self.gridLayout = QGridLayout(self.centralwidget)
-        self.gridLayout.setObjectName(u"gridLayout")
+        self.verticalLayout_3 = QVBoxLayout(self.centralwidget)
+        self.verticalLayout_3.setObjectName(u"verticalLayout_3")
+        self.horizontalLayout_3 = QHBoxLayout()
+        self.horizontalLayout_3.setObjectName(u"horizontalLayout_3")
         self.openGLWidget = AstroDisplay(self.centralwidget)
         self.openGLWidget.setObjectName(u"openGLWidget")
         sizePolicy = QSizePolicy(QSizePolicy.Preferred, QSizePolicy.Preferred)
@@ -38,9 +40,9 @@ class Ui_MainWindow(object):
         sizePolicy.setVerticalStretch(200)
         sizePolicy.setHeightForWidth(self.openGLWidget.sizePolicy().hasHeightForWidth())
         self.openGLWidget.setSizePolicy(sizePolicy)
-        self.openGLWidget.setMinimumSize(QSize(200, 200))
+        self.openGLWidget.setMinimumSize(QSize(600, 200))
 
-        self.gridLayout.addWidget(self.openGLWidget, 0, 0, 1, 1)
+        self.horizontalLayout_3.addWidget(self.openGLWidget)
 
         self.verticalLayout = QVBoxLayout()
         self.verticalLayout.setObjectName(u"verticalLayout")
@@ -53,9 +55,16 @@ class Ui_MainWindow(object):
 
         self.dateTimeEdit = QDateTimeEdit(self.centralwidget)
         self.dateTimeEdit.setObjectName(u"dateTimeEdit")
+        sizePolicy1 = QSizePolicy(QSizePolicy.Minimum, QSizePolicy.Fixed)
+        sizePolicy1.setHorizontalStretch(0)
+        sizePolicy1.setVerticalStretch(0)
+        sizePolicy1.setHeightForWidth(self.dateTimeEdit.sizePolicy().hasHeightForWidth())
+        self.dateTimeEdit.setSizePolicy(sizePolicy1)
+        self.dateTimeEdit.setMinimumSize(QSize(100, 0))
         self.dateTimeEdit.setMaximumTime(QTime(23, 59, 59))
         self.dateTimeEdit.setCurrentSection(QDateTimeEdit.YearSection)
         self.dateTimeEdit.setDisplayFormat(u"yyyy-MM-ddThh:mm:ssZ")
+        self.dateTimeEdit.setCalendarPopup(True)
         self.dateTimeEdit.setTimeSpec(Qt.LocalTime)
 
         self.horizontalLayout.addWidget(self.dateTimeEdit)
@@ -63,34 +72,85 @@ class Ui_MainWindow(object):
 
         self.verticalLayout.addLayout(self.horizontalLayout)
 
+        self.horizontalLayout_2 = QHBoxLayout()
+        self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
+        self.horizontalSpacer = QSpacerItem(40, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
+
+        self.horizontalLayout_2.addItem(self.horizontalSpacer)
+
         self.pushButton_play_and_pause = QPushButton(self.centralwidget)
         self.pushButton_play_and_pause.setObjectName(u"pushButton_play_and_pause")
+        sizePolicy2 = QSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+        sizePolicy2.setHorizontalStretch(0)
+        sizePolicy2.setVerticalStretch(0)
+        sizePolicy2.setHeightForWidth(self.pushButton_play_and_pause.sizePolicy().hasHeightForWidth())
+        self.pushButton_play_and_pause.setSizePolicy(sizePolicy2)
+        self.pushButton_play_and_pause.setLayoutDirection(Qt.LeftToRight)
 
-        self.verticalLayout.addWidget(self.pushButton_play_and_pause)
+        self.horizontalLayout_2.addWidget(self.pushButton_play_and_pause)
+
+        self.horizontalSpacer_2 = QSpacerItem(40, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
+
+        self.horizontalLayout_2.addItem(self.horizontalSpacer_2)
+
+
+        self.verticalLayout.addLayout(self.horizontalLayout_2)
 
         self.verticalSpacer = QSpacerItem(20, 40, QSizePolicy.Minimum, QSizePolicy.Expanding)
 
         self.verticalLayout.addItem(self.verticalSpacer)
 
 
-        self.gridLayout.addLayout(self.verticalLayout, 0, 1, 1, 1)
+        self.horizontalLayout_3.addLayout(self.verticalLayout)
+
+
+        self.verticalLayout_3.addLayout(self.horizontalLayout_3)
+
+        self.tabWidget = QTabWidget(self.centralwidget)
+        self.tabWidget.setObjectName(u"tabWidget")
+        sizePolicy3 = QSizePolicy(QSizePolicy.Preferred, QSizePolicy.Minimum)
+        sizePolicy3.setHorizontalStretch(0)
+        sizePolicy3.setVerticalStretch(0)
+        sizePolicy3.setHeightForWidth(self.tabWidget.sizePolicy().hasHeightForWidth())
+        self.tabWidget.setSizePolicy(sizePolicy3)
+        self.tabWidget.setMinimumSize(QSize(200, 50))
+        self.tab = QWidget()
+        self.tab.setObjectName(u"tab")
+        self.verticalLayout_2 = QVBoxLayout(self.tab)
+        self.verticalLayout_2.setObjectName(u"verticalLayout_2")
+        self.tableView = QTableView(self.tab)
+        self.tableView.setObjectName(u"tableView")
+
+        self.verticalLayout_2.addWidget(self.tableView)
+
+        self.tabWidget.addTab(self.tab, "")
+        self.tab_2 = QWidget()
+        self.tab_2.setObjectName(u"tab_2")
+        self.tabWidget.addTab(self.tab_2, "")
+
+        self.verticalLayout_3.addWidget(self.tabWidget)
 
         self.pushButton_2 = QPushButton(self.centralwidget)
         self.pushButton_2.setObjectName(u"pushButton_2")
 
-        self.gridLayout.addWidget(self.pushButton_2, 2, 0, 1, 1)
+        self.verticalLayout_3.addWidget(self.pushButton_2)
 
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QMenuBar(MainWindow)
         self.menubar.setObjectName(u"menubar")
-        self.menubar.setGeometry(QRect(0, 0, 1240, 22))
+        self.menubar.setGeometry(QRect(0, 0, 1348, 22))
         MainWindow.setMenuBar(self.menubar)
         self.statusbar = QStatusBar(MainWindow)
         self.statusbar.setObjectName(u"statusbar")
         MainWindow.setStatusBar(self.statusbar)
 
         self.retranslateUi(MainWindow)
+        self.dateTimeEdit.dateTimeChanged.connect(MainWindow.change_simulation_time)
         self.pushButton_play_and_pause.clicked.connect(MainWindow.play_simulation)
+        self.tableView.doubleClicked.connect(MainWindow.update_watchercatcher_selection_in_table)
+
+        self.tabWidget.setCurrentIndex(0)
+
 
         QMetaObject.connectSlotsByName(MainWindow)
     # setupUi
@@ -99,6 +159,8 @@ class Ui_MainWindow(object):
         MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
         self.label.setText(QCoreApplication.translate("MainWindow", u"Time:", None))
         self.pushButton_play_and_pause.setText(QCoreApplication.translate("MainWindow", u"\u25b6/||", None))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab), QCoreApplication.translate("MainWindow", u"Tab 1", None))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_2), QCoreApplication.translate("MainWindow", u"Tab 2", None))
         self.pushButton_2.setText(QCoreApplication.translate("MainWindow", u"PushButton", None))
     # retranslateUi
 
--- example/pyside/test.py
+++ example/pyside/test.py
@@ -1,8 +1,9 @@
 from datetime import datetime
 import astrolibrary
 
-from PySide6.QtWidgets import QApplication, QMainWindow
-from PySide6.QtCore import QTimer, Signal, Slot, QDateTime
+from PySide6.QtWidgets import QApplication, QMainWindow, QAbstractItemView, QHeaderView
+from PySide6.QtCore import QTimer, Signal, Slot, QDateTime, Qt
+from PySide6.QtGui import QStandardItemModel, QStandardItem
 
 from opengl_ui import Ui_MainWindow
 from space_objects import SpaceObjects
@@ -15,18 +16,131 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         super(MainWindow, self).__init__(*args, **kwargs)
         self.setupUi(self)
 
+        self.astro_client = astrolibrary.Client(
+            "Y8HSpeoKt+10sYVL7pRJum2lBg8XFfWOu+LVyN0Y26+5l7EO3WXTbGipnlkgkmPi"
+        )
+        self.space_objects = SpaceObjects(self.astro_client.tle_API.get_recent_tles())
+        self.watchercatcher_modal = QStandardItemModel()
+
         self.simulation_timer = QTimer()
         self.simulation_timer.timeout.connect(self.increase_simulation_time)
         self.simulation_time = QDateTime.currentDateTime()
         self.dateTimeEdit.setDateTime(self.simulation_time)
         self.simulation_unit_time = 100
-        self.space_objects = SpaceObjects(spacemap.tle_API.get_recent_tles())
+
+        self.map_row_index_to_watchercatcher = dict()
+
+        self.load_watchercatcher()
+
+    def load_watchercatcher(self):
+        status_list = self.astro_client.watcher_catcher_API.get_requests_status_list()
+
+        result = []
+        for status in status_list["data"]:
+            result.append(
+                self.astro_client.watcher_catcher_API.get_predicted_result(
+                    status["_id"]
+                )
+            )
+        print(result)
+        self.tableView.resizeRowsToContents()
+        self.tableView.setSelectionBehavior(QAbstractItemView.SelectRows)
+        self.tableView.setSelectionMode(QAbstractItemView.SingleSelection)
+
+        NUM_COL_WATCHERCATCHER = 7
+        self.watchercatcher_modal.setColumnCount(NUM_COL_WATCHERCATCHER)
+        self.watchercatcher_modal.setHeaderData(0, Qt.Horizontal, "Apex Latitude (deg)")
+        self.watchercatcher_modal.setHeaderData(
+            1, Qt.Horizontal, "Apex Longitude (deg)"
+        )
+        self.watchercatcher_modal.setHeaderData(2, Qt.Horizontal, "Cone Range (km)")
+        self.watchercatcher_modal.setHeaderData(
+            3, Qt.Horizontal, "Cone Field of View (deg)"
+        )
+        self.watchercatcher_modal.setHeaderData(
+            4, Qt.Horizontal, "Start Time of Timeline"
+        )
+        self.watchercatcher_modal.setHeaderData(
+            5, Qt.Horizontal, "End Time of Timeline"
+        )
+        self.watchercatcher_modal.setHeaderData(
+            6, Qt.Horizontal, "Downloaded Time Interval"
+        )
+
+        self.tableView.setModel(self.watchercatcher_modal)
+        self.tableView.setEditTriggers(QAbstractItemView.EditTrigger().NoEditTriggers)
+
+        header = self.tableView.horizontalHeader()
+
+        header.setSectionResizeMode(0, QHeaderView.ResizeMode.ResizeToContents)
+        header.setSectionResizeMode(1, QHeaderView.ResizeMode.ResizeToContents)
+        header.setSectionResizeMode(2, QHeaderView.ResizeMode.ResizeToContents)
+        header.setSectionResizeMode(3, QHeaderView.ResizeMode.ResizeToContents)
+        header.setSectionResizeMode(4, QHeaderView.ResizeMode.ResizeToContents)
+        header.setSectionResizeMode(5, QHeaderView.ResizeMode.ResizeToContents)
+        header.setSectionResizeMode(6, QHeaderView.ResizeMode.ResizeToContents)
+
+        curr_row = 0
+        for curr_data in result:
+            curr_item = QStandardItem(1, NUM_COL_WATCHERCATCHER)
+            self.watchercatcher_modal.insertRow(curr_row, curr_item)
+
+            apex_latitude_index = self.watchercatcher_modal.index(curr_row, 0)
+            apex_longitude_index = self.watchercatcher_modal.index(curr_row, 1)
+            cone_range_index = self.watchercatcher_modal.index(curr_row, 2)
+            cone_field_of_view_index = self.watchercatcher_modal.index(curr_row, 3)
+            start_time_of_timeline_index = self.watchercatcher_modal.index(curr_row, 4)
+            end_time_of_timelin_index = self.watchercatcher_modal.index(curr_row, 5)
+            downloaded_time_of_used_TLE_index = self.watchercatcher_modal.index(
+                curr_row, 6
+            )
+            watching_time_interval_index = self.watchercatcher_modal.index(curr_row, 7)
+
+            self.watchercatcher_modal.setData(
+                apex_latitude_index, curr_data.apex_latitude
+            )
+            self.watchercatcher_modal.setData(
+                apex_longitude_index, curr_data.apex_longitude
+            )
+            self.watchercatcher_modal.setData(cone_range_index, curr_data.cone_range)
+            self.watchercatcher_modal.setData(
+                cone_field_of_view_index, curr_data.cone_field_of_view
+            )
+            self.watchercatcher_modal.setData(
+                start_time_of_timeline_index, curr_data.start_time_of_timeline
+            )
+            self.watchercatcher_modal.setData(
+                end_time_of_timelin_index, curr_data.end_time_of_timeline
+            )
+            self.watchercatcher_modal.setData(
+                downloaded_time_of_used_TLE_index, curr_data.downloaded_time_of_used_TLE
+            )
+            self.watchercatcher_modal.setData(
+                watching_time_interval_index, curr_data.watching_time_interval
+            )
+
+            # temporary code, we can't handle QItem now.
+            self.map_row_index_to_watchercatcher[curr_row] = curr_data
+            curr_row += 1
+
+    def update_watchercatcher_selection_in_table(self, selected_row):
+        print(selected_row.row())
+        row = selected_row.row()
+        print(self.map_row_index_to_watchercatcher[row])
+        
 
     def increase_simulation_time(self):
         # change_time_by_given_increment()
         self.simulation_time = self.simulation_time.addMSecs(self.simulation_unit_time)
-
         self.dateTimeEdit.setDateTime(self.simulation_time)
+        self.update_simulation_time()
+        # self.update()
+
+    def change_simulation_time(self):
+        self.simulation_time = self.dateTimeEdit.dateTime()
+        self.update_simulation_time()
+
+    def update_simulation_time(self):
         start = time.time()  # 시작
         coordinates = self.space_objects.position_vector_of_objects_at_moment(
             self.simulation_time.toPython()
@@ -35,8 +149,9 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         for coordinate in coordinates:
             self.openGLWidget.add_point(coordinate)
         print(f"propagate: {time.time()-start:.4f} sec")  # 종료와 함께 수행시간 출력
+        start = time.time()  # 시작
         self.openGLWidget.update()
-        # self.update()
+        print(f"update: {time.time()-start:.4f} sec")  # 종료와 함께 수행시간 출력
 
     def play_simulation(self):
         if self.simulation_timer.isActive() == False:
@@ -47,10 +162,6 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 
 
 if __name__ == "__main__":
-    spacemap = astrolibrary.Client(
-        "Y8HSpeoKt+10sYVL7pRJum2lBg8XFfWOu+LVyN0Y26+5l7EO3WXTbGipnlkgkmPi"
-    )
-
     app = QApplication()
     window = MainWindow()
 
