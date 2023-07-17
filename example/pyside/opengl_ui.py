14b23903dcde8ab27e78eee144e67e816ca94649
Seunghwan17 <shchoi.vdrc@gmail.com>
2023-07-17 14:25:12 +0900

    aaa

--- example/pyside/opengl_ui.py
+++ example/pyside/opengl_ui.py
@@ -3,7 +3,7 @@
 ################################################################################
 ## Form generated from reading UI file 'opengl.ui'
 ##
-## Created by: Qt User Interface Compiler version 6.5.0
+## Created by: Qt User Interface Compiler version 6.5.1
 ##
 ## WARNING! All changes made in this file will be lost when recompiling UI file!
 ################################################################################
@@ -15,10 +15,11 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QDateTimeEdit, QHBoxLayout, QHeaderView,
-    QLabel, QMainWindow, QMenuBar, QPushButton,
-    QSizePolicy, QSpacerItem, QStatusBar, QTabWidget,
-    QTableView, QVBoxLayout, QWidget)
+from PySide6.QtWidgets import (QApplication, QDateTimeEdit, QFormLayout, QHBoxLayout,
+    QHeaderView, QLabel, QMainWindow, QMenuBar,
+    QPushButton, QSizePolicy, QSpacerItem, QStatusBar,
+    QTabWidget, QTableView, QTreeView, QVBoxLayout,
+    QWidget)
 
 from astrodisplay import AstroDisplay
 
@@ -118,10 +119,35 @@ class Ui_MainWindow(object):
         self.tab.setObjectName(u"tab")
         self.verticalLayout_2 = QVBoxLayout(self.tab)
         self.verticalLayout_2.setObjectName(u"verticalLayout_2")
+        self.formLayout = QFormLayout()
+        self.formLayout.setObjectName(u"formLayout")
+        self.treeView = QTreeView(self.tab)
+        self.treeView.setObjectName(u"treeView")
+        sizePolicy4 = QSizePolicy(QSizePolicy.Fixed, QSizePolicy.Expanding)
+        sizePolicy4.setHorizontalStretch(0)
+        sizePolicy4.setVerticalStretch(0)
+        sizePolicy4.setHeightForWidth(self.treeView.sizePolicy().hasHeightForWidth())
+        self.treeView.setSizePolicy(sizePolicy4)
+
+        self.formLayout.setWidget(1, QFormLayout.LabelRole, self.treeView)
+
         self.tableView = QTableView(self.tab)
         self.tableView.setObjectName(u"tableView")
 
-        self.verticalLayout_2.addWidget(self.tableView)
+        self.formLayout.setWidget(1, QFormLayout.FieldRole, self.tableView)
+
+        self.label_2 = QLabel(self.tab)
+        self.label_2.setObjectName(u"label_2")
+
+        self.formLayout.setWidget(0, QFormLayout.LabelRole, self.label_2)
+
+        self.label_3 = QLabel(self.tab)
+        self.label_3.setObjectName(u"label_3")
+
+        self.formLayout.setWidget(0, QFormLayout.FieldRole, self.label_3)
+
+
+        self.verticalLayout_2.addLayout(self.formLayout)
 
         self.tabWidget.addTab(self.tab, "")
         self.tab_2 = QWidget()
@@ -159,6 +185,8 @@ class Ui_MainWindow(object):
         MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
         self.label.setText(QCoreApplication.translate("MainWindow", u"Time:", None))
         self.pushButton_play_and_pause.setText(QCoreApplication.translate("MainWindow", u"\u25b6/||", None))
+        self.label_2.setText(QCoreApplication.translate("MainWindow", u"File Archive", None))
+        self.label_3.setText(QCoreApplication.translate("MainWindow", u"RFI results", None))
         self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab), QCoreApplication.translate("MainWindow", u"Tab 1", None))
         self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_2), QCoreApplication.translate("MainWindow", u"Tab 2", None))
         self.pushButton_2.setText(QCoreApplication.translate("MainWindow", u"PushButton", None))
